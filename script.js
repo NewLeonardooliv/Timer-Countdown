26822886942bacfc766378df286da0059aca9149
Leonardo <Leonardo_oliveira7@hotmail.com>
2022-03-11 11:31:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const minutes = 5;
+let countdownSeconds = minutes * 60;
+
+function renderCoundown(minutes, seconds) {
+  const minutesToPad = String(minutes).padStart(2, "0");
+  const secondsToPad = String(seconds).padStart(2, "0");
+  console.log(`${minutesToPad}:${secondsToPad}`);
+
+  const minutesSpan = document.getElementById("minutes");
+  const secondsSpan = document.getElementById("seconds");
+
+  minutesSpan.innerHTML = minutesToPad;
+  secondsSpan.innerHTML = secondsToPad;
+}
+
+setInterval(function () {
+  if (countdownSeconds <= 0) {
+    renderCoundown(0, 0);
+    return;
+  }
+
+  let minutes = Math.floor(countdownSeconds / 60);
+  let seconds = countdownSeconds % 60;
+  renderCoundown(minutes, seconds);
+
+  countdownSeconds--;
+}, 1000);
