48826dda12d633af63871fbc275284e9fcbdce1a
Leonardo <Leonardo_oliveira7@hotmail.com>
2022-03-12 12:08:36 -0300

    .

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ const minutes = 5;
 let countdownSeconds = minutes * 60;
 
 function renderCoundown(minutes, seconds) {
-  const minutesToPad = String(minutes).padStart(2, "0");
-  const secondsToPad = String(seconds).padStart(2, "0");
-  console.log(`${minutesToPad}:${secondsToPad}`);
+    const minutesToPad = String(minutes).padStart(2, "0");
+    const secondsToPad = String(seconds).padStart(2, "0");
+    console.log(`${minutesToPad}:${secondsToPad}`);
 
-  const minutesSpan = document.getElementById("minutes");
-  const secondsSpan = document.getElementById("seconds");
+    const minutesSpan = document.getElementById("minutes");
+    const secondsSpan = document.getElementById("seconds");
 
-  minutesSpan.innerHTML = minutesToPad;
-  secondsSpan.innerHTML = secondsToPad;
+    minutesSpan.innerHTML = minutesToPad;
+    secondsSpan.innerHTML = secondsToPad;
 }
 
-setInterval(function () {
-  if (countdownSeconds <= 0) {
-    renderCoundown(0, 0);
-    return;
-  }
+setInterval(function() {
+    if (countdownSeconds <= 0) {
+        renderCoundown(0, 0);
+        return;
+    }
 
-  let minutes = Math.floor(countdownSeconds / 60);
-  let seconds = countdownSeconds % 60;
-  renderCoundown(minutes, seconds);
+    let minutes = Math.floor(countdownSeconds / 60);
+    let seconds = countdownSeconds % 60;
+    renderCoundown(minutes, seconds);
 
-  countdownSeconds--;
+    countdownSeconds--;
 }, 1000);
