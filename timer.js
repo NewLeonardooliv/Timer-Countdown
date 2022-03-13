1d1ec580f5d7975ad8bb70767515227c62689a7d
Leonardo <Leonardo_oliveira7@hotmail.com>
2022-03-13 18:54:41 -0300

    .

--- timer.js
+++ timer.js
@@ -2,9 +2,11 @@ const minutes = 1;
 let seconds = minutes * 60;
 
 function timer(minutes, seconds) {
+    //adiciona o 0 se o valor for menor que 10 e transforma em string
     const showMinutes = minutes < 10 ? minutes = '0' + String(minutes) : minutes = String(minutes);
     const showSeconds = seconds < 10 ? seconds = '0' + String(seconds) : seconds = String(seconds);
 
+    //envia os valores para os ID's do HTML
     const minutesTime = document.getElementById("minutes");
     const secondsTime = document.getElementById("seconds");
 
@@ -12,12 +14,15 @@ function timer(minutes, seconds) {
     secondsTime.innerHTML = showSeconds;
 }
 
+//transforma o setInterval em uma variavel para ser puxada na hora do clearInterval
 const showTime = setInterval(function () {
+    //se o valor for menor que 0 o tempo irá parar em "00:00"
     if (seconds <= 0) {
         timer(0, 0);
         return;
     }
 
+    //transforma o tempo da contagem para minutos e segundos
     const nMinutes = parseInt(seconds / 60);
     const nSeconds = seconds % 60;
     timer(nMinutes, nSeconds);
@@ -26,10 +31,12 @@ const showTime = setInterval(function () {
 
 }, 1000);
 
+//funcao para pausar o tempo
 function pause() {
     clearInterval(showTime);
 }
+//funcao para parar o tempo e ficar 00:00
 function cancel() {
     clearInterval(showTime);
-    clearInterval(timer(0,0));
+    clearInterval(timer(0, 0));
 }
